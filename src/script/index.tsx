27b2a8f2be65d0b7211a0a1bc08cfe767297f669
flame <fl4m3@pm.me>
2023-04-22 22:48:47 +0800

    Restructure asset folder

--- src/script/index.tsx
+++ src/script/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Flame from './Flame.tsx'
-import '../asset/index.css'
+import '../asset/css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
