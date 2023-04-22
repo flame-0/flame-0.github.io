23b392332c4a551685d9990965097af300d6b5c0
flame <fl4m3@pm.me>
2023-04-22 21:36:19 +0800

    Initial commit

---
+++ src/script/Flame.tsx
@@ -0,0 +1,10 @@
+export default function Flame() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-nord-0">
+      <div className="w-content">
+        <p className="leading-4 text-nord-6">I craft codes and ask computers to try them.</p>
+        <p className="text-nord-9">— <a href="https://github.com/flame-0" className="hover:underline">Flame</a></p>
+      </div>
+    </div>
+  )
+}
---
+++ src/script/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Flame from './Flame.tsx'
+import '../asset/index.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Flame />
+  </React.StrictMode>,
+)
